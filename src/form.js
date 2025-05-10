62daa61de357fafaa013132b115c27e04861a26b
osigbemhe2020 <62507389+osigbemhe2020@users.noreply.github.com>
2025-05-10 14:49:33 +0100

    Add files via upload

---
+++ src/form.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { CiGlobe } from "react-icons/ci";
+
+const Formcont = () => {
+  return (
+    <>
+      <div className="newflex"> 
+        <div class="field">
+          <input type="text" name="name" placeholder="search" className='top-search'/>
+          <CiGlobe className="icon"/>
+        </div>
+        <div><button className='top-button'> Sort By</button></div>
+      </div>
+    </>
+  )
+}
+
+export default Formcont
---
+++ src/main.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Formcont from './form.js'
+import Body from './body.js'
+
+const Main = () => {
+  return (
+    <>
+     <div className='main'>
+        <Formcont/>
+        <Body />
+     </div>
+    </>
+  )
+}
+
+export default Main
